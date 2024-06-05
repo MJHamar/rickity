f449df88acc7a843872898b337737aaed86828a2
MJHamar <Miklos.Hamar@warwick.ac.uk>
2024-06-05 21:46:10 +0200

    file sync service is not in a very good shape

--- file_sync.py
+++ file_sync.py
@@ -1,4 +1,5 @@
 import os
+import re
 import time
 import json
 import shutil
@@ -9,7 +10,7 @@ from threading import Timer
 
 # Setup logging
 logging.basicConfig(
-    filename='file_sync.log',
+#    filename='/Users/hamarmiklos/file_sync/file_sync.log',
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s'
 )
@@ -17,39 +18,70 @@ logging.basicConfig(
 # Helper function to create hard link
 def create_hard_link(src, dst):
     try:
+        logging.debug(f"Creating hard link: {src} -> {dst}")
         if not os.path.exists(dst):
             os.link(src, dst)
-            logging.info(f"Hard link created: {src} -> {dst}")
+            logging.info("Hard link created:\n%s\n->\n%s", src, dst)
     except FileExistsError:
         logging.info(f"Hard link already exists: {dst}")
     except Exception as e:
         logging.error(f"Error creating hard link: {e}")
 
 # Ensure all files in source directory have hard links in target directory
-def ensure_hard_links(source, target, ignore):
+def ensure_hard_links(source, target, include_=[], include_patterns_=[], ignore_patterns_=[]):
     for root, dirs, files in os.walk(source):
         for filename in files:
-            rel_path = os.path.relpath(os.path.join(root, filename), source)
-            if rel_path not in ignore:
-                source_path = os.path.join(root, filename)
-                target_path = os.path.join(target, rel_path)
-                if not os.path.exists(os.path.dirname(target_path)):
-                    os.makedirs(os.path.dirname(target_path))
-                create_hard_link(source_path, target_path)
+            # include if matches pattern
+            src = os.path.join(root, filename)
+            rel_path = os.path.relpath(src, source)
+            dst = os.path.join(target, rel_path)
+            include = any(re.match(pattern, rel_path) for pattern in include_)\
+                if len(include_) > 0 else False
+            include_patterns = any(re.match(pattern, rel_path) for pattern in include_patterns_)\
+                if len(include_patterns_) > 0 else False
+            ignore_patterns = any(re.match(pattern, rel_path) for pattern in ignore_patterns_)\
+                if len(ignore_patterns_) > 0 else False
+            # priority is include > ignore_patterns > include_patterns
+            do_include = include or include_patterns or not ignore_patterns
+            if do_include:
+                logging.debug(
+                    f"Source: {src}, Target: {dst}, DoInclude: {do_include}; Include Patterns: {include_patterns}, Ignore Patterns: {ignore_patterns}, Include: {include}")
+
+            if do_include:
+                if not os.path.exists(dst):
+                    if not os.path.exists(os.path.dirname(dst)):
+                        os.makedirs(os.path.dirname(dst))
+                    create_hard_link(src, dst)
 
 # Handler for file system events
 class FileHandler(FileSystemEventHandler):
-    def __init__(self, source, target, ignore):
+    def __init__(self, source, target, include=[], include_patterns=[], ignore_patterns=[]):
         self.source = source
         self.target = target
-        self.ignore = ignore
+        self.include = [re.compile(pattern) for pattern in include]
+        self.include_patterns = [re.compile(pattern) for pattern in include_patterns]
+        self.ignore_patterns = [re.compile(pattern) for pattern in ignore_patterns]
+        logging.info("Source: %s, Target: %s, Include: %s, Include Patterns: %s, Ignore Patterns: %s",
+                     source, target, include, include_patterns, ignore_patterns)
 
     def on_created(self, event):
         if not event.is_directory:
+            logging.debug(f"File created: {event.src_path}")
             source_path = event.src_path
             rel_path = os.path.relpath(source_path, self.source)
             target_path = os.path.join(self.target, rel_path)
-            if rel_path not in self.ignore:
+
+            include = any(re.match(pattern, rel_path) for pattern in self.include)\
+                if len(self.include) > 0 else False
+            include_patterns = any(re.match(pattern, rel_path) for pattern in self.include_patterns)\
+                if len(self.include_patterns) > 0 else False
+            ignore_patterns = any(re.match(pattern, rel_path) for pattern in self.ignore_patterns)\
+                if len(self.ignore_patterns) > 0 else False
+            # priority is include > ignore_patterns > include_patterns
+            do_include = include or include_patterns or not ignore_patterns
+            logging.debug(f"Source: {source_path}, Target: {target_path}, Include: {do_include}")
+
+            if do_include:
                 if source_path.startswith(self.source):
                     if not os.path.exists(os.path.dirname(target_path)):
                         os.makedirs(os.path.dirname(target_path))
@@ -60,10 +92,44 @@ class FileHandler(FileSystemEventHandler):
                     shutil.move(source_path, self.source)
                     create_hard_link(os.path.join(self.source, rel_path), target_path)
 
+    def on_deleted(self, event):
+        # If a file is deleted in the source directory, delete the hard link in the target directory
+        if not event.is_directory:
+            logging.debug(f"File deleted: {event.src_path}")
+            source_path = event.src_path
+            rel_path = os.path.relpath(source_path, self.source)
+            target_path = os.path.join(self.target, rel_path)
+
+            include = any(re.match(pattern, rel_path) for pattern in self.include)\
+                if len(self.include) > 0 else False
+            include_patterns = any(re.match(pattern, rel_path) for pattern in self.include_patterns)\
+                if len(self.include_patterns) > 0 else False
+            ignore_patterns = any(re.match(pattern, rel_path) for pattern in self.ignore_patterns)\
+                if len(self.ignore_patterns) > 0 else False
+            # priority is include > ignore_patterns > include_patterns
+            do_include = include or include_patterns or not ignore_patterns
+            logging.debug(f"Source: {source_path}, Target: {target_path}, Include: {do_include}")
+
+            if do_include:
+                if source_path.startswith(self.source):
+                    if os.path.exists(target_path):
+                        os.remove(target_path)
+                        logging.info(f"Hard link deleted: {target_path}")
+                elif source_path.startswith(self.target):
+                    if os.path.exists(os.path.join(self.source, rel_path)):
+                        os.remove(os.path.join(self.source, rel_path))
+                        logging.info(f"Hard link deleted: {os.path.join(self.source, rel_path)}")
+
 def periodic_check(directories):
+    logging.debug("Periodic check...")
     for directory_pair in directories:
         logging.info(f"Periodically checking {directory_pair['source']} and {directory_pair['target']}")
-        ensure_hard_links(directory_pair['source'], directory_pair['target'], directory_pair.get('ignore', []))
+        ensure_hard_links(source=directory_pair['source'],
+                          target=directory_pair['target'],
+                          include_=directory_pair.get('include', []),
+                          include_patterns_=directory_pair.get('include_patterns', []),
+                          ignore_patterns_=directory_pair.get('ignore_patterns', []))
+        logging.info("Periodic check for % and %s done.")
 
 def main(config_file):
     logging.info("Starting file sync...")
@@ -71,10 +137,9 @@ def main(config_file):
         config = json.load(file)
 
     directories = config['directories']
-    
+    logging.info(f"Directories: {json.dumps(directories, indent=2)}")
     # Ensure initial hard links
-    for directory_pair in directories:
-        ensure_hard_links(directory_pair['source'], directory_pair['target'], directory_pair.get('ignore', []))
+    periodic_check(directories)
 
     event_handlers = []
     observers = []
@@ -82,8 +147,10 @@ def main(config_file):
     for directory_pair in directories:
         source = directory_pair['source']
         target = directory_pair['target']
-        ignore = directory_pair.get('ignore', [])
-        event_handler = FileHandler(source, target, ignore)
+        include = directory_pair.get('include', [])
+        include_patterns = directory_pair.get('include_patterns', [])
+        ignore_patterns = directory_pair.get('ignore_patterns', [])
+        event_handler = FileHandler(source, target, include, include_patterns, ignore_patterns)
         observer = Observer()
         observer.schedule(event_handler, source, recursive=True)
         observer.schedule(event_handler, target, recursive=True)
@@ -93,9 +160,10 @@ def main(config_file):
 
     # Start periodic check
     interval = 300  # Check every 5 minutes
-    Timer(interval=interval,
+    periodic = Timer(interval=interval,
           function=periodic_check,
-          args=[directories]).start()
+          args=[directories])
+    periodic.start()
 
     try:
         while True:
@@ -107,7 +175,8 @@ def main(config_file):
             observer.stop()
         for observer in observers:
             observer.join()
+        periodic.cancel()
 
 if __name__ == "__main__":
-    config_file = "directories.json"  # Path to your JSON configuration file
+    config_file = "config.json"  # Path to your JSON configuration file
     main(config_file)
