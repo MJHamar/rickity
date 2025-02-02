7d7dc17391840c186a77c58a2c0244af9a6237c7
MJHamar <Miklos.Hamar@warwick.ac.uk>
2025-02-02 09:31:19 +0100

    google cloud endpoint config for webswrver

--- frontend/js/common.js
+++ frontend/js/common.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://0.0.0.0:2233';  // Empty string for same-origin requests
+const API_URL = 'http://34.118.115.23:2233';
 
 // Utility functions used across both pages
 const formatDate = (date) => {
